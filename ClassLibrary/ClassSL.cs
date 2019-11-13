07c1f8b7a3b702dbaf7dce7dce6ad8c2e8fbec3d
Daniel Lemus Zuluaga <chani55510@hotmail.com>
2019-11-13 11:17:06 -0500

    Directivas Preprocesador

---
+++ ClassLibrary/ClassSL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StandardLibrary
+{
+    public class ClassSL
+    {
+        Console.WriteLine("Clase funcionando en Libreria .Net Standard");
+
+#if MOBILE
+            Android.Widget.Button btn = new Android.Widget.Button(null);
+            btn.Text = "Ejemplo";
+#else
+            System.Windows.Forms.Button btn = new System.Windows.Forms.Button();
+#endif
+
+    }
+}
